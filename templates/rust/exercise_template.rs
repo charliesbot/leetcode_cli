04b02cb0d5d22d0a3ede7c465ec4eb76abdea5b0
Charlie L <charliesbot@gmail.com>
2024-07-17 21:14:21 -0700

    add config for rust

--- templates/rust/exercise_template.rs
+++ templates/rust/exercise_template.rs
@@ -10,3 +10,13 @@ pub struct Solution {}
 
  __PROBLEM_DEFAULT_CODE__
 
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test___PROBLEM_NAME_FORMATTED__() {
+        assert_eq!(1, 1, "Basic assertion to confirm test setup");
+    }
+}
+
