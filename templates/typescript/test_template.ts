95e2a711158bfa1e4fe3ce7a4fd0afd1873ab9c5
Charlie L <charliesbot@gmail.com>
2025-07-23 18:59:01 -0700

    add the templates folder back

---
+++ templates/typescript/test_template.ts
@@ -0,0 +1,7 @@
+import assert from 'node:assert';
+import test from 'node:test';
+import { __PROBLEM_NAME_FORMATTED__ } from './__EXERCISE_FILE_NAME__';
+
+test('__PROBLEM_NAME_FORMATTED__', t => {
+  assert.equal(1, 1);
+});
