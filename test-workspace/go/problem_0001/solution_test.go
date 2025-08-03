6493a038841ab695f965645fb7d9352af79817fe
Charlie L <charliesbot@gmail.com>
2025-08-02 20:36:52 -0700

    add go support

--- test-workspace/go/problem_0001/solution_test.go
+++
@@ -1,8 +0,0 @@
-package 
-
-import "testing"
-
-func TestTwoSum(t *testing.T) {
-	// TODO: Add test cases
-	t.Log("Test not implemented yet")
-}
--- test-workspace/test-workspace/go/problem_0001/solution_test.go
+++
@@ -1,8 +0,0 @@
-package problem_0001
-
-import "testing"
-
-func TestTwoSum(t *testing.T) {
-	// TODO: Add test cases
-	t.Log("Test not implemented yet")
-}
